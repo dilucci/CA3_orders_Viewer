f1af837d93188d7b31101d6074c471cdebdb6d60
Seb <dilucci@gmail.com>
2014-10-31 19:29:30 +0100

    Opdateret menu til at linke til azure

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function() {
-    mongoose.connect("mongodb://gruppe4.azurewebsites.net/northwind");
+    mongoose.connect("mongodb://gruppe4.azurewebsites.dk/northwind");
 
     mongoose.connection.once("open", function() {
         console.log("Connected to Northwind");
